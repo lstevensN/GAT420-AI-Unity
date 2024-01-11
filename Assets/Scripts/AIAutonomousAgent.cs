42d9ed59a666b80c4dec47909f02d381bf5429a0
lstevensN <lstevens@student.neumont.edu>
2024-01-11 13:54:37 -0700

    Seek!

--- Assets/Scripts/AIAutonomousAgent.cs
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -8,11 +8,28 @@ public class AIAutomousAgent : AIAgent
 
     private void Update()
     {
+        // seek
         var gameObjects = perception.GetGameObjects();
-
-        foreach (var go in gameObjects)
+        if (gameObjects.Length > 0 )
         {
-            Debug.DrawLine(transform.position, go.transform.position, Color.red);
+            movement.ApplyForce(Seek(gameObjects[0]));
         }
+
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10 ), new Vector3(10, 10, 10));
+    }
+
+    private Vector3 Seek(GameObject target)
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        return GetSteeringForce(direction);
+    }
+
+    private Vector3 GetSteeringForce(Vector3 direction)
+    {
+        Vector3 desired = direction.normalized * movement.maxSpeed;
+        Vector3 steer = desired - movement.Velocity;
+        Vector3 force = Vector3.ClampMagnitude(steer, movement.maxForce);
+
+        return force;
     }
 }
