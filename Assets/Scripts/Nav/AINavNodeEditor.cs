45f672e7ca486b857f1e4a74723527330d21a6f2
lstevensN <lstevens@student.neumont.edu>
2024-01-30 14:01:09 -0700

    Navigation World

--- Assets/Scripts/Nav/AINavNodeEditor.cs
+++ Assets/Scripts/Nav/AINavNodeEditor.cs
@@ -8,135 +8,164 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class NavNodeEditor : MonoBehaviour
 {
-	[SerializeField] GameObject navNodePrefab;
-	[SerializeField] LayerMask layerMask;
-	[SerializeField] bool active = true;
+    [SerializeField] GameObject navNodePrefab;
+    [SerializeField] LayerMask layerMask;
 
-	private Vector3 position = Vector3.zero;
-	private bool spawnable = false;
-	private AINavNode navNode = null;
-	private AINavNode activeNavNode = null;
-	
-	private void OnEnable()
-	{
-		if (!Application.isEditor)
-		{
-			Destroy(this);
-		}
-		SceneView.duringSceneGui += OnScene;
-	}
+    private Vector3 position = Vector3.zero;
+    private bool spawnable = false;
+    private AINavNode navNode = null;
+    private AINavNode activeNavNode = null;
 
-	private void OnDisable()
-	{
-		SceneView.duringSceneGui -= OnScene;
-	}
+    private bool active = false;
+    private bool mousePressed = false;
 
-	void OnScene(SceneView scene)
-	{
-		if (!active) return;
+    private void OnEnable()
+    {
+        if (!Application.isEditor)
+        {
+            Destroy(this);
+        }
+        SceneView.duringSceneGui += OnScene;
+    }
 
-		Event e = Event.current;
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnScene;
+    }
 
-		// get scene mouse position
-		Vector3 mousePosition = e.mousePosition;
-		mousePosition.y = scene.camera.pixelHeight - mousePosition.y * EditorGUIUtility.pixelsPerPoint;
-		mousePosition.x *= EditorGUIUtility.pixelsPerPoint;
+    void OnScene(SceneView scene)
+    {
+        Event e = Event.current;
 
-		Ray ray = scene.camera.ScreenPointToRay(mousePosition);
+        // set editor active when space is held down
+        if (e.isKey && e.keyCode == KeyCode.Space)
+        {
+            if (e.type == EventType.KeyDown) active = true;
+            if (e.type == EventType.KeyUp) active = false;
+        }
 
-		// check mouse over spawn/nav layer
-		if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
-		{
-			position = hitInfo.point;
+        // return if not active, reset nodes
+        if (!active)
+        {
+            navNode = null;
+            activeNavNode = null;
+            return;
+        }
 
-			if (hitInfo.collider.gameObject.TryGetComponent<AINavNode>(out navNode))
-			{
-				if (activeNavNode == null)
-				{
-					Selection.activeGameObject = navNode.gameObject;
-				}
-				spawnable = false;
-			}
-			else spawnable = true;
-		}
-		else
-		{
-			navNode = null;
-			spawnable = false;
-		}
+        // scene does not pass mouse up event, work around to get mouse up event type
+        HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
 
-		// if spawnable and mouse pressed, create nav node
-		if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Alpha1)
-		{
-			if (spawnable && navNode == null && activeNavNode == null) Instantiate(navNodePrefab, position, Quaternion.identity, transform);
-			if (navNode != null && activeNavNode == null)
-			{
-				activeNavNode = navNode;
-				navNode = null;
-			}
-		}
-		// add connection to active nav node
-		if (e.type == EventType.KeyUp && e.keyCode == KeyCode.Alpha1)
-		{
-			if (activeNavNode != null && navNode != null && activeNavNode != navNode)
-			{
-				if (!activeNavNode.neighbors.Contains(navNode))
-				{
-					activeNavNode.neighbors.Add(navNode);
-				}
+        var controlID = GUIUtility.GetControlID(FocusType.Passive);
+        var eventType = e.GetTypeForControl(controlID);
 
-				if (!navNode.neighbors.Contains(activeNavNode))
-				{
-					navNode.neighbors.Add(activeNavNode);
-				}
-			}
-			activeNavNode = null;
-		}
+        // check for node or spawn ray hit
+        if (e.isMouse && (e.type == EventType.MouseMove || e.type == EventType.MouseDrag))
+        {
+            // get scene mouse position
+            Vector3 mousePosition = e.mousePosition;
+            mousePosition.y = scene.camera.pixelHeight - mousePosition.y * EditorGUIUtility.pixelsPerPoint;
+            mousePosition.x *= EditorGUIUtility.pixelsPerPoint;
 
-		// delete nav node
-		if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Alpha2)
-		{
-			if (navNode != null)
-			{
-				DestroyImmediate(navNode.gameObject);
-			}
-		}
+            // compute ray from mouse position
+            Ray ray = scene.camera.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
+            {
+                position = hitInfo.point;
+                // if not over node in scene, set spawnable to true
+                spawnable = !(hitInfo.collider.gameObject.TryGetComponent<AINavNode>(out navNode));
+                e.Use();
+            }
+            else
+            {
+                // if not over spawn or node layer then reset navNode
+                navNode = null;
+                spawnable = false;
+            }
 
-		// use the event
-		e.Use();
-	}
+        }
 
-	private void OnDrawGizmos()
-	{
-		if (!active) return;
+        // check mouse down
+        if (eventType == EventType.MouseDown)
+        {
+            // if spawnable, create nav node
+            if (spawnable && navNode == null && activeNavNode == null)
+            {
+                Instantiate(navNodePrefab, position, Quaternion.identity, transform);
+            }
+            // if nav node is selected then set active nav node to nav node (used for connections)
+            if (navNode != null && activeNavNode == null)
+            {
+                activeNavNode = navNode;
+                navNode = null;
+            }
+            e.Use();
+        }
 
-		if (spawnable && navNode == null)
-		{
-			Gizmos.color = Color.blue;
-			Gizmos.DrawWireSphere(position, 1);
-		}
-		if (navNode != null && navNode != activeNavNode)
-		{
-			Gizmos.color = Color.green;
-			Gizmos.DrawWireSphere(navNode.gameObject.transform.position, 1);
-		}
-		if (activeNavNode != null)
-		{
-			Gizmos.color = Color.red;
-			Gizmos.DrawWireSphere(activeNavNode.gameObject.transform.position, 1.5f);
-			Gizmos.DrawLine(activeNavNode.gameObject.transform.position, position);
-		}
+        // check mouse up
+        if (eventType == EventType.MouseUp)
+        {
+            // if there's an active node and over a different node, create connection
+            if (activeNavNode != null && navNode != null && activeNavNode != navNode)
+            {
+                // connect from active nav node to nav node, if not already connected
+                if (!activeNavNode.neighbors.Contains(navNode))
+                {
+                    activeNavNode.neighbors.Add(navNode);
+                }
 
-		// draw connections
-		var nodes = AINavNode.GetAINavNodes();
-		foreach (AINavNode node in nodes)
-		{
-			foreach (AINavNode neighbors in node.neighbors)
-			{
-				Gizmos.color = Color.green;
-				Gizmos.DrawLine(node.transform.position, neighbors.transform.position);
-			}
-		}
+                // connect from nav node to active nav node, if not already connected
+                if (!navNode.neighbors.Contains(activeNavNode))
+                {
+                    navNode.neighbors.Add(activeNavNode);
+                }
+            }
+            activeNavNode = null;
+            e.Use();
+        }
 
-	}
+        // delete nav node
+        if (e.isKey && e.keyCode == KeyCode.Minus)
+        {
+            if (navNode != null)
+            {
+                DestroyImmediate(navNode.gameObject);
+            }
+            e.Use();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!active) return;
+
+
+        if (spawnable && navNode == null)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(position, 1);
+        }
+        if (navNode != null && navNode != activeNavNode)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(navNode.gameObject.transform.position, 1);
+        }
+        if (activeNavNode != null)
+        {
+            Gizmos.color = (navNode != null && activeNavNode != navNode) ? Color.green : Color.red;
+            Gizmos.DrawWireSphere(activeNavNode.gameObject.transform.position, 1.5f);
+            Gizmos.DrawLine(activeNavNode.gameObject.transform.position + Vector3.up, position + Vector3.up);
+        }
+
+        // draw connections
+        var nodes = AINavNode.GetAINavNodes();
+        foreach (AINavNode node in nodes)
+        {
+            foreach (AINavNode neighbors in node.neighbors)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawLine(node.transform.position + Vector3.up, neighbors.transform.position + Vector3.up);
+            }
+        }
+
+    }
 }
